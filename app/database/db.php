06cc742ae35b82ca4ebbc413626c96394e07787e
mukhametali <m.kussaiynov1996@gmail.com>
2023-01-10 17:53:55 +0600

    Дорабатываем функцию запросов к базе данных

--- app/database/db.php
+++ app/database/db.php
@@ -9,23 +9,76 @@ function tt($value){
 
 
 }
-
-function selectAll($table){
-    global $pdo;
-    $sql = "SELECT * FROM $table";
-    $query = $pdo->prepare($sql);
-    $query->execute();
+// Проверка выполнения запроса к БД
+function dbCheckError($query){
     $errInfo = $query->errorInfo();
-
     if($errInfo[0] !== PDO::ERR_NONE){
         echo $errInfo[2];
         exec();
     }
+    return true;
+}
 
+// Запрос на получения данных c одной таблицы
+function selectAll($table, $params = []){
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+
+    if(!empty($params)){
+       $i = 0;
+       foreach ($params as $key => $value){
+           if (!is_numeric($value)){
+               $value = "'" .$value. "'";
+           }
+           if ($i === 0){
+               $sql = $sql . " WHERE $key = $value";
+           }else{
+               $sql = $sql . " AND $key = $value";
+           }
+           $i++;
+       }
+    }
+
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
     return $query->fetchAll();
 }
 
-tt(selectAll('users'));
 
+// Запрос на получения одной строки с выбранной таблицы
+function selectOne($table, $params = []){
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+
+    if(!empty($params)){
+        $i = 0;
+        foreach ($params as $key => $value){
+            if (!is_numeric($value)){
+                $value = "'" .$value. "'";
+            }
+            if ($i === 0){
+                $sql = $sql . " WHERE $key = $value";
+            }else{
+                $sql = $sql . " AND $key = $value";
+            }
+            $i++;
+        }
+    }
+//    $sql = $sql . " LIMIT 1";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
+    return $query->fetch();
+}
+
+
+$params = [
+    'admin' => 1,
+    'email' => 'ali@gmail.com',
+];
+
+//tt(selectAll('users', $params));
+tt(selectOne('users'));
 
 
